36f2be8fbfbdbdfb9d88dab01200a5dd35ab846e
divyanat2009 <69719463+divyanat2009@users.noreply.github.com>
2020-10-28 14:22:17 -0500

    changes to format query

--- index.js
+++ index.js
@@ -4,40 +4,43 @@ var map = new mapboxgl.Map({
 container: 'map',
 style: 'mapbox://styles/mapbox/dark-v10',
 zoom:1.5,
-center:[0,20]
 });
 
 const url="https://api.covid19api.com/country/${userInput}/status/confirmed"
 
 function init(){
   populateCountries();
+  submitForm();
 }
 function submitForm(){
   $("#app-form").submit(e=>{
       e.preventDefault();
-  const userInput= $("#countries").val();  
+  const userInput= $("#countryInput").val();  
+  //console.log(userInput);
   getCasesResult(userInput);
   });  
 }
 function populateCountries(){
   countryList.forEach((country)=> {
-  $("#countries").append(`<option>${country}</option>` )
+  $("#countryInput").append(`<option>${country}</option>` )
   })
 }
-function formatQueryParams(){
+function formatQueryParams(params){
   const queryItems= Object.keys(params).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)  
   return queryItems.join("&");
 }
 function getCasesResult(query){
   const params={
-    from: $('#from'),
-    to: $('#to')
+    from: $('#from').val(),
+    to: $('#to').val()
 };
+
 const queryString= formatQueryParams(params);
 const searchURL= url + '?' + queryString;
+console.log(searchURL);
 fetch(searchURL)
   .then(response =>response.json())
-  .then(response => renderCountryResults(response.data))
+  .then(response => renderCountryResults(response.length))
   .catch(err =>alert(err));
 }
 function renderCountryResults(countryList){

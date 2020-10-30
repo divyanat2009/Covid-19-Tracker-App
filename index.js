aa646d1afbeb74ac7056dd57b3472caae5b00ddc
divyanat2009 <69719463+divyanat2009@users.noreply.github.com>
2020-10-29 23:05:25 -0500

    added in-line comments

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Generate the MapBox
 const mapbox_token="pk.eyJ1IjoiZGl2eWFuYXQyMDA5IiwiYSI6ImNrZ3NjZW9mbTBhdmMyd3J6OGp2czNiZnAifQ.MGN9qZ3Mku6-FSlgJqgbEQ";
 mapboxgl.accessToken = mapbox_token;
 var map = new mapboxgl.Map({
@@ -5,15 +6,16 @@ container: 'map',
 style: 'mapbox://styles/mapbox/dark-v10',
 zoom:1.5,
 });
-
+// Generate the URL
 const url="https://api.covid19api.com/country/${userInput}/status/confirmed"
-
+//Initialize the function
 function init(){
   $( "#from" ).datepicker();
   $( "#to" ).datepicker();
   populateCountries();
   submitForm();  
 }
+//Create a watch to prevent form submission
 function submitForm(){
   $("#app-form").submit(e=>{
     e.preventDefault();
@@ -23,15 +25,18 @@ function submitForm(){
   getCasesResult(userInput, fromDateInput, toDateInput);
   });  
 }
+//Get list of countries
 function populateCountries(){
   countryList.forEach((country)=> {
   $("#countryInput").append(`<option>${country}</option>` )
   })
 }
+//Format the query parameters
 function formatQueryParams(params){
   const queryItems= Object.keys(params).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)  
   return queryItems.join("&");
 }
+// Get Cases Result
 function getCasesResult(userInput, fromDateInput, toDateInput){
   const url=`https://api.covid19api.com/country/${userInput}/status/confirmed`;  
   const params={
@@ -46,19 +51,29 @@ fetch(searchURL)
   .then(response => renderCountryResults(response))
   .catch(err =>alert(err));
 }
+// Render the results
 function renderCountryResults(countryList){
     $("#results").empty();    
     let totalCount = getTotalCount(countryList);
     $("#results").append(`<h3>Total confirmed cases: ${totalCount}</h3>`);
+    //Create marker on map
     let lon = getLonLat(countryList, "Lon")
     let lat = getLonLat(countryList, "Lat")
+    mapboxgl.accessToken = mapbox_token;
+    var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/dark-v10',
+    zoom:1.5,
+    });
     var marker = new mapboxgl.Marker()
     .setLngLat([lon, lat])
-    .addTo(map);  
+    .addTo(map);
 }  
+//Generate marker for each country
 function getLonLat(countryList, value){
   return parseFloat(countryList[0][`${value}`]);
 }
+//Get total cases count
 function getTotalCount(countryList){
   let totalCount = 0;
   countryList.forEach(country => {

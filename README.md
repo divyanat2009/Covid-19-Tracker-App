bbe6d260459abe9e0f537415f96466aafab49f1f
divyanat2009 <69719463+divyanat2009@users.noreply.github.com>
2020-11-05 11:06:37 -0600

    populated countries from API, added merker using new API

--- README.md
+++ README.md
@@ -5,6 +5,5 @@ This app helps user track Covid-19 confirmed cases across countries.
 The results are found using Covid19 API and MapBox.
 User selects a country and date-from, date-to options. This search gives user total confirmed cases count.
 Screenshots of the app:
-![Screenshot (6)](https://user-images.githubusercontent.com/69719463/97760493-4602ae80-1ad1-11eb-8098-23f5d0faeb3f.png)
-![Screenshot (8)](https://user-images.githubusercontent.com/69719463/97760494-469b4500-1ad1-11eb-8072-4f024a22a506.png)
+
 
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+"use strict"
 //Generate the MapBox
 const mapbox_token="pk.eyJ1IjoiZGl2eWFuYXQyMDA5IiwiYSI6ImNrZ3NjZW9mbTBhdmMyd3J6OGp2czNiZnAifQ.MGN9qZ3Mku6-FSlgJqgbEQ";
 mapboxgl.accessToken = mapbox_token;
@@ -17,16 +18,47 @@ function submitForm(){
   $("#app-form").submit(e=>{  
   e.preventDefault();
   const userInput= $("#countryInput").val();  
+  fetch(`https://api.covid19api.com/country/${userInput}/status/confirmed/live?from=2020-03-01T00:00:00Z&to=2020-04-01T00:00:00Z`)
+  .then (response => response.json())
+  .then(response => {
+    console.log(response[0].Lat, response[0].Lon);
+    const mapbox_token="pk.eyJ1IjoiZGl2eWFuYXQyMDA5IiwiYSI6ImNrZ3NjZW9mbTBhdmMyd3J6OGp2czNiZnAifQ.MGN9qZ3Mku6-FSlgJqgbEQ";
+mapboxgl.accessToken = mapbox_token;
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/dark-v10',
+zoom:1.5,
+});
+    var marker = new mapboxgl.Marker()
+.setLngLat([response[0].Lon, response[0].Lat])
+.addTo(map);
+  })
   getCasesResult(userInput);
   });  
 }
+function getRequest(url){
+  
+}
 //Get list of countries
 function populateCountries(){
-  countryList.forEach((country)=> {
-  $("#countryInput").append(`<option value='${country}'>${country}</option>` )
+  fetch("https://covid-193.p.rapidapi.com/countries", {
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-key": "997b4b4b44mshffb7d7452a7563ap1d23d6jsn758cf6f706f3",
+      "x-rapidapi-host": "covid-193.p.rapidapi.com"
+    }
   })
+  .then(response => response.json())
+  .then(response =>
+    {
+    
+    
+      response.response.forEach((country)=> {
+        $("#countryInput").append(`<option value='${country}'>${country}</option>` )
+        })        
+    }).catch(err =>alert(err)); 
 }
-// Get Cases Result
+//Get Cases Result
 function getCasesResult(userInput){
 const url=`https://rapidapi.p.rapidapi.com/statistics`;  
 
@@ -42,54 +74,39 @@ fetch(url, {
   {
     renderResult(response, userInput);
   })
-.catch(err => {
-	console.error(err);
+.catch(err => {alert(err);
 });
 }
 
 function renderResult(response, userInput)
 {
   $("#results").html(``);
-    let countryData = response.response.find(x=> x.country == userInput);
-    $("#results").append(`<h4>View Info: </h4>`);
-    Object.entries(countryData).forEach(([k,v])=> {
-      if(k == "cases")
+  $("#results").removeClass("hidden");
+  let countryData = response.response.find(x=> x.country == userInput);    
+  $("#results").append(`<h4>View Info: </h4>`);    
+  Object.entries(countryData).forEach(([k,v])=> {
+    if(k == "cases")
       {
-        Object.entries(countryData.cases).forEach(([k,v])=> {
-          $("#results").append(`<h5>${k} : ${v} </h5>`);
-        });
-        return;
+       Object.entries(countryData.cases).forEach(([k,v])=> {
+       $("#results").append(`<h5>${k} : ${v} </h5>`);
+       });
+      return;
       }
       else if(k == "deaths")
       {
         Object.entries(countryData.deaths).forEach(([k,v])=> {
-          $("#results").append(`<h5>${k} : ${v} </h5>`);
+        $("#results").append(`<h5>${k} : ${v} </h5>`);
         });
         return;
       }
       if(k == "tests")
       {
         Object.entries(countryData.tests).forEach(([k,v])=> {
-          $("#results").append(`<h5>${k} : ${v} </h5>`);
+        $("#results").append(`<h5>${k} : ${v} </h5>`);
         });
         return;
       }
       $("#results").append(`<h5>${k} : ${v} </h5>`);
-    })
-    //Create marker on map
-    let countryMark = getCountry(countryList, "country")
-    //let lat = getLonLat(countryList, "Lat")
-    mapboxgl.accessToken = mapbox_token;
-    var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/dark-v10',
-    zoom:1.5,
-    });
-    var marker = new mapboxgl.Marker()
-    .setCountry(countryMark)
-    .addTo(map);   
-}
-function getCountry(countryList, value){
-  return parseFloat(countryList[0][`${value}`]);
+    })    
 }
 $(init);        
